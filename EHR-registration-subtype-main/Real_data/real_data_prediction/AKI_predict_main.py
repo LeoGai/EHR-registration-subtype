934c6f49bce66bb02c6d9483ffd07f63c98890f5
Shiyi-J <51498500+Shiyi-J@users.noreply.github.com>
2025-12-06 22:38:36 -0500

    Add files via upload
    Added code for subgroup analysis.

--- EHR-registration-subtype-main/Real_data/real_data_prediction/AKI_predict_main.py
+++ EHR-registration-subtype-main/Real_data/real_data_prediction/AKI_predict_main.py
@@ -8,39 +8,62 @@ import torch
 curve_range = 21
 n_repeat = 500
 te_sz = 0.3
-raw_data = pd.read_csv('AKI_DATA/AKI_data_recovered.csv') # change path as needed
-raw_data = raw_data[['ID', 'Feature', 'Time', 'Value']]
-raw_data = raw_data.rename(columns={'ID':'SUBJECT_ID', 'Feature':'feature', 'Time':'time', 'Value':'response'})
-raw_data['time'] = round(raw_data['time'])
+raw_data = pd.read_csv(path) # change path as needed
 
-def filter_data(raw_data, curve_range):
+def filter_data(raw_data, curve_range, name):
     subid = np.unique(raw_data['SUBJECT_ID'])
     data = []
     for i, id in enumerate(subid):
         temp = raw_data[raw_data['SUBJECT_ID']==id]
-        data.append(temp[temp['time'] < curve_range])
+        if name == 'AKI_base_reg':
+            data.append(temp[temp['register_time'] < curve_range])
+        else:
+            data.append(temp[temp['time'] < curve_range])
     data = pd.concat(data)
     return data, subid
 
-def report_results(log_dict, length):
-    for metric in ['auc','pre','rec','acc','auprc']:
-        val = log_dict[metric]
-        c25 = np.percentile(val, 2.5)
-        c97 = np.percentile(val, 97.5)
-        print('n_days:', length, 'metric:', metric, 'mean:', np.mean(val), '95CI:', c25, c97)
+def report_results(log_dict, pred_dict, length, name):
+    # subgroup
+    final_df = []
+    groups = ['age_g1', 'age_g2', 'gender_g1', 'gender_g2']
+    for gr in groups:
+        gr_sc = []
+        for metric in ['auc', 'pre', 'rec', 'acc', 'auprc']:
+            val = log_dict[gr][metric]
+            mu = np.mean(val)
+            c25 = np.percentile(val, 2.5)
+            c97 = np.percentile(val, 97.5)
+            gr_sc.extend([mu, c25, c97])
+        final_df.append(gr_sc)
+    final_df = pd.DataFrame(final_df, columns=['auc_mu','auc_25','auc_97','pre_mu','pre_25','pre_97',
+        'rec_mu','rec_25','rec_97','acc_mu','acc_25','acc_97','auprc_mu','auprc_25','auprc_97'], index=groups)
+    #final_df.to_csv(path)
+    # prediction
+    pred_df = []
+    for metric_p in ['auc', 'pre', 'rec', 'acc', 'auprc']:
+        val_p = pred_dict[metric_p]
+        mu_p = np.mean(val_p)
+        c25_p = np.percentile(val_p, 2.5)
+        c97_p = np.percentile(val_p, 97.5)
+        pred_df.append([metric_p, mu_p, c25_p, c97_p])
+    pred_df = pd.DataFrame(pred_df, columns=['metric','mean','c25','c97'])
+    #pred_df.to_csv(path)
     return ;
 
-def wrap_fcn(args, name='recover2'):
-    data, subid = filter_data(raw_data, curve_range)
-    scores = test_prediction(data, True, None, subid, curve_range, te_sz, 'LR', n_repeat, 2, 10, 0.01, None, 200, args, name)
-    report_results(scores, args)
-    torch.save(scores, 'AKI_DATA/'+name+'_500rep_'+str(args)+'.pt')
+def wrap_fcn(args, name='AKI_recover2'): # name options: AKI_origin, AKI_base_reg, AKI_recover, and AKI_recover2
+    if name != 'AKI_base_reg':
+        raw_data = raw_data[['ID', 'Feature', 'Time', 'Value']]
+        raw_data = raw_data.rename(columns={'ID':'SUBJECT_ID', 'Feature':'feature', 'Time':'time', 'Value':'response'})
+        raw_data['time'] = round(raw_data['time'])
+    data, subid = filter_data(raw_data, curve_range, name)
+    subid = np.unique(data['SUBJECT_ID'])
+    sub_sc, pred_sc = test_prediction(data, True, None, subid, curve_range, te_sz, 'LR', n_repeat, 2, 10, 0.01, None, 200, args, name)
+    #torch.save(sub_sc, path)
+    report_results(sub_sc, pred_sc, args, name)
     return ;
 
-def main(num_job=4):
-    combinations = [3, 4]
-    with Pool(num_job) as pool:
-        results = pool.map(wrap_fcn, combinations)
+def main():
+    wrap_fcn(4)
 
 if __name__ == '__main__':
     main()
--- EHR-registration-subtype-main/Real_data/real_data_prediction/predict_utils.py
+++ EHR-registration-subtype-main/Real_data/real_data_prediction/predict_utils.py
@@ -1,4 +1,6 @@
 import sys
+import copy
+from tqdm import tqdm
 import math
 import random
 import pandas as pd
@@ -17,6 +19,22 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
+def compute_auc(y_te, y_proba, length, plot=False):
+    n_class = np.unique(y_te)
+    auroc, auprc = [], []
+    plt.figure(figsize=(10,8))
+    for i in n_class:
+        y_true = y_te==i
+        fpr, tpr, thres = metrics.roc_curve(y_true, y_proba[:,int(i)])
+        pre, rec, thresholds = precision_recall_curve(y_true, y_proba[:,int(i)])
+        auroc.append(metrics.auc(tpr, fpr))
+        auprc.append(metrics.auc(rec, pre))
+        plt.plot(rec, pre, label='Class'+str(int(i)))
+    if plot:
+        plt.legend(loc='best')
+        #plt.savefig(path)
+    return np.mean(auroc), np.mean(auprc)
+
 def compute_feature(reg, df_lb, length, time, df_info):
     fea_name = np.unique(reg['feature'])
     df = pd.DataFrame()
@@ -28,10 +46,49 @@ def compute_feature(reg, df_lb, length, time, df_info):
         df = pd.concat([df, pd.DataFrame(fea_data)], axis=1)
     return df.values, df_lb['cluster'].values
 
+def metric_compute(y_proba, y_hat, y_te, length):
+    auc = roc_auc_score(y_te, y_proba[:,1])
+    pre = metrics.precision_score(y_te, y_hat, average='macro')
+    rec = metrics.recall_score(y_te, y_hat, average='macro')
+    acc = metrics.accuracy_score(y_te, y_hat)
+    _, auprc = compute_auc(y_te, y_proba, length, plot=False)
+    return auc, pre, rec, acc, auprc
+
+def subgroup_analysis(stay_list, y_proba, y_hat, y_te, length, name, run_id):
+    df_demo = pd.read_csv('patients.csv')
+    df_icu = pd.read_csv('icustays.csv')
+    age_list, gender_list = [], []
+    for stay in stay_list:
+        subj = df_icu.loc[df_icu['stay_id']==stay,'subject_id'].iloc[0]
+        df_age = df_demo.loc[df_demo['subject_id']==subj,'anchor_age']
+        age = df_age.values[0]
+        gender = df_demo.loc[df_demo['subject_id']==subj,'gender'].values[0] # M or F
+        age_list.append(age)
+        gender_list.append(gender)
+    age_list = np.array(age_list)
+    gender_list = np.array([0 if x=='F' else 1 for x in gender_list])
+    # age
+    age_g1 = (y_proba[age_list < 60], y_hat[age_list < 60], y_te[age_list < 60])
+    age_g2 = (y_proba[age_list >= 60], y_hat[age_list >= 60], y_te[age_list >= 60])
+    #torch.save((age_g1, age_g2), path)
+    age_sc_g1 = metric_compute(age_g1[0], age_g1[1], age_g1[2], length)
+    age_sc_g2 = metric_compute(age_g2[0], age_g2[1], age_g2[2], length)
+    # gender
+    gender_g1 = (y_proba[gender_list==0], y_hat[gender_list==0], y_te[gender_list==0])
+    gender_g2 = (y_proba[gender_list==1], y_hat[gender_list==1], y_te[gender_list==1])
+    #torch.save((gender_g1, gender_g2), path)
+    gender_sc_g1 = metric_compute(gender_g1[0], gender_g1[1], gender_g1[2], length)
+    gender_sc_g2 = metric_compute(gender_g2[0], gender_g2[1], gender_g2[2], length)
+    return age_sc_g1, age_sc_g2, gender_sc_g1, gender_sc_g2
+
 def test_prediction(df, lb, icd_df, subj_id, curve_range, te_sz, method, n_repeat, lmbd, beta, gamma, K, t_max, length, name):
+    pred_dict = {'auc':[], 'pre':[], 'rec':[], 'acc':[], 'auprc':[]}
     log_dict = {'auc':[], 'pre':[], 'rec':[], 'acc':[], 'auprc':[]}
-    X_reg, _, _, _, _ = preprocess(df, curve_range, icd_df, beta, gamma, lmbd, t_max, lb, plot=False)
-    X_reg['time'] = X_reg['time'].astype(int)
+    groups = ['age_g1', 'age_g2', 'gender_g1', 'gender_g2']
+    subgr_dict = {g: copy.deepcopy(log_dict) for g in groups}
+    if name != 'AKI_base_reg':
+        X_reg, _, _, _, _ = preprocess(df, curve_range, icd_df, beta, gamma, lmbd, t_max, lb, plot=False)
+        X_reg['time'] = X_reg['time'].astype(int)
     # create labels
     df_lb = pd.read_csv('AKI_DATA/AKI_raw_s3_labels.csv')
     df_lb = df_lb[['ID','s3_lb']]
@@ -41,32 +98,56 @@ def test_prediction(df, lb, icd_df, subj_id, curve_range, te_sz, method, n_repea
     df_lb = df_lb[df_lb['SUBJECT_ID'].isin(subj_id)]
     gr0_subj = df_lb.loc[df_lb['cluster']==0, 'SUBJECT_ID']
     gr1_subj = df_lb.loc[df_lb['cluster']==1, 'SUBJECT_ID']
-    for i in range(n_repeat):
+    for i in tqdm(range(n_repeat)):
         # start prediction
-        tr0, te0 = train_test_split(gr0_subj, test_size=te_sz) #random_state=42
-        tr1, te1 = train_test_split(gr1_subj, test_size=te_sz)
+        tr0, te0 = train_test_split(gr0_subj, test_size=te_sz, random_state=i) #random_state=42
+        tr1, te1 = train_test_split(gr1_subj, test_size=te_sz, random_state=i)
         tr_subj, te_subj = pd.concat([tr0, tr1]), pd.concat([te0, te1])
-        X_train, X_test = X_reg[X_reg['SUBJECT_ID'].isin(tr_subj)], X_reg[X_reg['SUBJECT_ID'].isin(te_subj)]
+        if name == 'AKI_base_reg':
+            X_train, X_test = df[df['SUBJECT_ID'].isin(tr_subj)], df[df['SUBJECT_ID'].isin(te_subj)]
+        else:
+            X_train, X_test = X_reg[X_reg['SUBJECT_ID'].isin(tr_subj)], X_reg[X_reg['SUBJECT_ID'].isin(te_subj)]
         y_train, y_test = df_lb[df_lb['SUBJECT_ID'].isin(tr_subj)], df_lb[df_lb['SUBJECT_ID'].isin(te_subj)]
-        X_tr, y_tr = compute_feature(X_train, y_train, length, 'time', None)
-        X_te, y_te = compute_feature(X_test, y_test, length, 'time', None)
+        if name == 'AKI_base_reg':
+            X_tr, y_tr = compute_feature(X_train, y_train, length, 'register_time', None)
+            X_te, y_te = compute_feature(X_test, y_test, length, 'register_time', None)
+        else:
+            X_tr, y_tr = compute_feature(X_train, y_train, length, 'time', None)
+            X_te, y_te = compute_feature(X_test, y_test, length, 'time', None)
+
         if method == 'LR':
             clf = LogisticRegression().fit(X_tr, y_tr)
             y_proba = clf.predict_proba(X_te)
-            auc = roc_auc_score(y_te, y_proba[:,1])
             y_hat = np.argmax(y_proba, axis=1)
+            #'''
+            # subgroup analysis
+            age_sc_g1, age_sc_g2, gender_sc_g1, gender_sc_g2 = subgroup_analysis(y_test['SUBJECT_ID'], y_proba, y_hat, y_te,
+                    length, name, i)
+            for gr in groups:
+                for j, metric in enumerate(['auc', 'pre', 'rec', 'acc', 'auprc']):
+                    if gr == 'age_g1':
+                        subgr_dict[gr][metric].append(age_sc_g1[j])
+                    elif gr == 'age_g2':
+                        subgr_dict[gr][metric].append(age_sc_g2[j])
+                    elif gr == 'gender_g1':
+                        subgr_dict[gr][metric].append(gender_sc_g1[j])
+                    elif gr == 'gender_g2':
+                        subgr_dict[gr][metric].append(gender_sc_g2[j])
+                    else:
+                        print('Group name not in the list!')
+            #'''
+            #'''
+            auc = roc_auc_score(y_te, y_proba[:,1]) #multi_class='ovo'
             pre = metrics.precision_score(y_te, y_hat, average='macro')
             rec = metrics.recall_score(y_te, y_hat, average='macro')
             acc = metrics.accuracy_score(y_te, y_hat)
-            auroc, auprc = compute_auc(y_te, y_proba, length, plot=False)
-            #torch.save(clf, 'AKI_DATA/'+name+'_'+str(length)+'.pt')
-            cm = metrics.plot_confusion_matrix(clf, X_te, y_te, normalize=None)
-            #cm.figure_.savefig('AKI_DATA/'+name+'_'+str(length)+'_cmat.pdf')
-            log_dict['auc'].append(auc)
-            log_dict['pre'].append(pre)
-            log_dict['rec'].append(rec)
-            log_dict['acc'].append(acc)
-            log_dict['auprc'].append(auprc)
+            _, auprc = compute_auc(y_te, y_proba, length, plot=False)
+            pred_dict['auc'].append(auc)
+            pred_dict['pre'].append(pre)
+            pred_dict['rec'].append(rec)
+            pred_dict['acc'].append(acc)
+            pred_dict['auprc'].append(auprc)
+            #'''
         else:
             print('Method not defined yet!')
-    return log_dict
+    return subgr_dict, pred_dict
